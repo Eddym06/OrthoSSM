3567afb5c8eaeff6fb7280f2a1e6afee6cc055a8
Eddym06 <eddym06@github.com>
2026-05-14 22:11:45 -0400

    Limpieza

--- fix_imports.py
+++
@@ -1,7 +0,0 @@
-with open("chimera_experiment/advanced_chimera.py", "r") as f:
-    text = f.read()
-
-text = "import torch\nimport torch.nn as nn\nimport torch.nn.functional as F\nimport math\n\n" + text
-
-with open("chimera_experiment/advanced_chimera.py", "w") as f:
-    f.write(text)
--- fix_test.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-print("Waiting for triton compilation on first run...")
